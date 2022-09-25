d124145aeae60a23056bf503b04f7b0534a11c9f
dipeshkumarsah98 <dipeshsah98@gmail.com>
2022-09-25 12:48:36 +0545

    adding custom fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,21 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        'major-mono': ["'Major Mono Display'", 'monospace'],
-        'nunito': ["'Nunito'", 'sans-serif'],
-        'merienda ': ["'Merienda One'", 'sans-serif'],
-        'raleway  ': ["'Raleway'", 'sans-serif'],
+        "major-mono": ["'Major Mono Display'", "monospace"],
+        nunito: ["'Nunito'", "sans-serif"],
+        "merienda ": ["'Merienda One'", "sans-serif"],
+        "raleway  ": ["'Raleway'", "sans-serif"],
+      },
+      fontSize: {
+        xxl: "10rem",
+        custom: "7.8rem",
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
