aeee3ba019800e6b872b0b085798fe6282bab732
dipeshkumarsah98 <dipeshsah98@gmail.com>
2022-09-25 12:20:09 +0545

    adding animation in work

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,58 +1,100 @@
-import { motion } from 'framer-motion'
-import React from 'react'
-import { Card } from './Card'
-
+import React from "react";
+import { motion } from "framer-motion";
+import { Card } from "./Card";
+import DogFinder from "../img/dog-finder.png";
+import MovieExplore from "../img/movie-explore.png";
+import SMS from "../img/student-management-system.png";
 
 export const Work = () => {
+  const item = {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.4, ease: "easeIn" } },
+  };
+  var description =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, veniam voluptate asperiores dolorum reprehenderit quaerat quod quae velit eius atque rerum debitis obcaecati corrupti necessitatibus sunt, earum sint possimus fuga voluptatibus officia iste reiciendis sed ratione. Enim cum at, ad, vero sequi veniam dolor eius, sed facilis neque molestias. Amet!";
 
+  const works = [
+    {
+      id: 1,
+      name: "Movie Explore",
+      description: description,
+      technology: [
+        "JavaScript",
+        "ReactJS",
+        "MobX",
+        "Axios",
+        "HTML5",
+        "Tailwind CSS",
+      ],
+      link: '"https://github.com/dipeshkumarsah98/E-commerical-API-django/tree/master"',
+      img: MovieExplore,
+    },
+    {
+      id: 2,
+      name: "Dog Finder",
+      description: description,
+      technology: [
+        "JavaScript",
+        "ReactJs",
+        "RRD",
+        "HTML5",
+        "Axios",
+        "JSON Web Token",
+      ],
+      link: "https://github.com/dipeshkumarsah98/DogFinder_ReactJS",
+      img: DogFinder,
+    },
+    {
+      id: 3,
+      name: "School Management System",
+      description: description,
+      technology: [
+        "Python",
+        "Javascript",
+        "ReactJs",
+        "Django",
+        "Rest Framework",
+        "HTML5",
+        "Axios",
+        "JSON Web Token",
+      ],
+      link: "https://github.com/dipeshkumarsah98/Student-management-system-backend",
+      img: SMS,
+    },
+  ];
 
-    const item = {
-        hidden: { opacity: 0, y: 20 },
-        show: { opacity: 1, y: 0, transition: { duration: 0.4, ease: 'easeIn' } },
-    }
-    const discription = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, veniam voluptate asperiores dolorum reprehenderit quaerat quod quae velit eius atque rerum debitis obcaecati corrupti necessitatibus sunt, earum sint possimus fuga voluptatibus officia iste reiciendis sed ratione. Enim cum at, ad, vero sequi veniam dolor eius, sed facilis neque molestias. Amet!"
+  return (
+    <motion.div className="my-20" id="work">
+      <motion.h1
+        variants={item}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.5 }}
+        className="font-nunito text-center text-5xl font-bold text-pink-300 lg:text-8xl md:text-left lg:my-30"
+      >
+        Recent Work
+      </motion.h1>
 
-    const eCommercialTech = ['Python', 'Django', 'Djnago Rest Framework', 'HTML6', 'Djoser', 'JSON Web Token']
-    const dogFiner = ['JavaScript', 'ReactJs', 'RRD', 'HTML6', 'Axios', 'JSON Web Token']
-    const schoolMangement = ['Python', 'Javascript', 'ReactJs', 'Django', 'Rest Framework', 'HTML6', 'Axios', 'JSON Web Token']
+      {works?.map((work) => (
+        <Card
+          item={item}
+          key={work.id}
+          image={work.img}
+          projectName={work.name}
+          projectDescription={work.description}
+          repoLink={work.link}
+          techItems={work.technology}
+        />
+      ))}
 
-    return (
-        <motion.div className='my-20' id='work' >
-            <motion.h1 variants={item}
-                initial='hidden'
-                whileInView="show"
-                viewport={{ once: true, amount: 0.5 }}
-                className='font-nunito text-center text-5xl font-bold text-pink-300 lg:text-8xl md:text-left lg:my-30'>Recent Work
-            </motion.h1>
-
-            <Card
-                className="my-5"
-                projectName={'E-commerical API'}
-                projectDescription={discription}
-                repoLink={'https://github.com/dipeshkumarsah98/E-commerical-API-django/tree/master'}
-                techItems={eCommercialTech}
-            />
-            <Card
-                projectName={'Dog Finder'}
-                projectDescription={discription}
-                repoLink={'https://github.com/dipeshkumarsah98/DogFinder_ReactJS'}
-                techItems={dogFiner}
-            />
-            <Card
-                projectName={'School Management System'}
-                projectDescription={discription}
-                repoLink={'https://github.com/dipeshkumarsah98/Student-management-system-backend'}
-                techItems={schoolMangement}
-            />
-
-            <motion.div
-                initial='hidden'
-                whileInView="show"
-                viewport={{ once: false, amount: 0.5 }}
-                className='text-center font-xl font-nunito  text-gray-300 my-10 md:text-3xl lg:text-5xl'>
-                More Project coming soon....
-            </motion.div>
-
-        </motion.div>
-    )
-}
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.5 }}
+        className="text-center font-xl font-nunito  text-gray-300 my-10 md:text-3xl lg:text-5xl"
+      >
+        More Project coming soon....
+      </motion.div>
+    </motion.div>
+  );
+};
